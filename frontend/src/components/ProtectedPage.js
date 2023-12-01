87c1db12e8dddb15b3127443a32661c347bca6d7
Ben Horan <ben.horan@mail.com>
2023-12-01 12:52:23 -0700

    Massive updates to docker compose

--- frontend/src/components/ProtectedPage.js
+++ frontend/src/components/ProtectedPage.js
@@ -11,7 +11,7 @@ const ProtectedPage = () => {
   const handleLogout = async () => {
     try {
       await axios.post(
-        "http://localhost:5000/logout",
+        "http://localhost:5000/api/logout",
         {},
         {
           headers: {
